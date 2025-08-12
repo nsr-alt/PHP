a41f1f2c40318625a1d7d3f01d68f20e09f2e8fa
nsr-alt <natasantosdosreis@gmail.com>
2025-08-12 16:28:43 -0300

    Add files via upload

--- Aula 6/operadores.php
+++ Aula 6/operadores.php
@@ -1,7 +1,14 @@
 <?php
-  
+  include "header.php";
+
+  echo "<h2>Sanções Aplicadadas - Comparação</h2>";
+
   $paisA = 10; //sanções aplicadas
   $paisB = 5;
  
-  echo "<p>O país A recebeu $paisA sanções, enquantos o país B recebeu $paisB.</p>";
+  echo "<p>O Rússia recebeu $paisA sanções, enquanto Kosovo recebeu $paisB.</p>";
+
+  echo "<p><strong>Total de sanções aplicadas</strong> ".($paisA + $paisB)."</p>";
+
+  include "footer.php";
 ?>
